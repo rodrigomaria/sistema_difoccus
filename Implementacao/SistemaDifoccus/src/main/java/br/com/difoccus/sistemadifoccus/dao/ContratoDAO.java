c2e9c07b827ec6e11c1be9111392e235506da602
andriuswill <awmdsilva@restinga.ifrs.edu.br>
2016-09-29 18:23:58 -0300

    Revert "Revert "Salvar erExclguir""
    This reverts commit 4335b76b7d6111781551cefff574e58fe1b8eeeb.

---
+++ Implementacao/SistemaDifoccus/src/main/java/br/com/difoccus/sistemadifoccus/dao/ContratoDAO.java
@@ -0,0 +1,7 @@
+package br.com.difoccus.sistemadifoccus.dao;
+
+import br.com.difoccus.sistemadifoccus.modelo.Contrato;
+
+public class ContratoDAO  extends  GenericDAO<Contrato>{
+    
+}
--- Implementacao/SistemaDifoccus/src/main/java/br/com/difoccus/sistemadifoccus/modelo/Funcionario.java
+++ Implementacao/SistemaDifoccus/src/main/java/br/com/difoccus/sistemadifoccus/modelo/Funcionario.java
@@ -7,7 +7,6 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="funcionarios")
 public class Funcionario {
     @Id
     @GeneratedValue
